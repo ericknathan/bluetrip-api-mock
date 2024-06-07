79cd19c8b719006c7fa859519f10ac13e0643092
ericknathan <erick.capito@hotmail.com>
2024-06-07 13:15:28 -0300

    feat: add local business images

--- src/http/routes/local-business.ts
+++ src/http/routes/local-business.ts
@@ -1,19 +1,26 @@
 import { FastifyInstance } from "fastify";
-import { z } from "zod";
 
+import { businessImages } from "../../lib/images";
 import { prisma } from "../../lib/prisma";
 
 export async function getLocalBusinessList(app: FastifyInstance) {
   app.get("/local-business", async (request, reply) => {
     const localBusiness = await prisma.localBusiness.findMany({});
 
-    const randomnizedLocalBusiness = localBusiness.sort(
-      () => Math.random() - 0.5
-    );
+    const randomnizedLocalBusiness = localBusiness
+      .sort(() => Math.random() - 0.5)
+      .sort((a, b) =>
+        ["ONG", "Passeios Turísticos", "Centro de mergulho"].includes(
+          a.businessCategory
+        ) ? -1 : 1
+      );
 
     return {
       message: "Comércios locais listados com sucesso!",
-      data: randomnizedLocalBusiness,
+      data: randomnizedLocalBusiness.map((business) => ({
+        ...business,
+        imageUrl: businessImages[business.businessCategory],
+      })),
     };
   });
 }
---
+++ src/lib/images.ts
@@ -0,0 +1,34 @@
+export const businessImages: Record<string, string> = {
+  Mercado:
+    "https://images.unsplash.com/photo-1635843134523-48433763fce7?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  Spa: "https://images.unsplash.com/photo-1492931307820-62fa5a68e0df?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  "Aluguel de Equipamentos":
+    "https://www.brasilmergulho.com/wp-content/uploads/2022/04/Loja.jpg",
+  "Esportes Aquáticos":
+    "https://nxboats.com.br/wp-content/uploads/2024/03/esportes-aquaticos.jpg",
+  Café: "https://media-cdn.tripadvisor.com/media/photo-s/1c/61/ca/eb/front-view-looking-toward.jpg",
+  Restaurante:
+    "https://diariodorio.com/wp-content/uploads/2021/06/WhatsApp-Image-2021-06-24-at-17.59.41.jpeg",
+  Yoga: "https://blog.belpiscinas.com.br/content/uploads/2019/09/yoga-na-piscina.jpg",
+  Fotografia:
+    "https://s2.glbimg.com/hrJzn7PHq9Yp8l8SftS_ibSbqMs=/620x620/top/e.glbimg.com/og/ed/f/original/2018/11/29/44803123_784369221913718_3641538171856126428_n.jpg",
+  "Centro de Mergulho":
+    "https://oceanosub.com/wp-content/uploads/2023/03/bannermobile.png",
+  Hotel:
+    "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0e/d3/f2/35/oceano-porto-hotel.jpg?w=700&h=-1&s=1",
+  Academia:
+    "https://maceioalgovbr.dhost.cloud/uploads/imagens/15-07-23-Academia-Praia-da-Avenida-Prado-SEMESP-Foto-Itawi-Albuquerque-17.jpg",
+  Aquário:
+    "https://www.hotelpaulistagarden.com.br/sao-paulo/wp-content/uploads/2014/05/aqu%C3%A1rio-de-s%C3%A3o-paulo.jpg",
+  "Passeios Turísticos":
+    "https://anda.jor.br/_next/image?url=https%3A%2F%2Fimages.anda.jor.br%2Fuploads%2F2022%2F09%2Funknown-19.jpeg&w=3840&q=75",
+  "Loja de Esportes":
+    "https://www.francisswim.com.br/fs/wp-content/uploads/2019/10/Portal-Francisswim-anuncia-parceria-com-a-Loja-NIZport-com-foco-nos-esportes-aqu%C3%A1ticos-em-Portugal-930x450.jpg",
+  ONG: "https://d3g532flx662j1.cloudfront.net/wp-content/uploads/2021/02/costacesar_blog_gremar-1024x475.jpg",
+  Cruzeiros:
+    "https://conteudo.imguol.com.br/c/entretenimento/7e/2022/10/24/icon-of-the-seas-1666633351171_v2_4x3.png",
+  Joalheria:
+    "https://www.comerciocomhistoria.gov.pt/wp-content/uploads/import/listings/3875_imagem2.jpg",
+  "Loja de Souvenirs":
+    "https://msc-oceancay-be.adacto.it/api/assets/ce23db45-36b2-4b3e-8b45-0b3201896179?width=1000",
+};
